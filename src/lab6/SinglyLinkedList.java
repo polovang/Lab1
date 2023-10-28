c107048fee5b851aa5aea7ac81585b9cfaba2258
win10pro <win10pro@DESKTOP-DG8QK26>
2023-10-28 10:35:28 -0700

    them

--- src/lab6/SinglyLinkedList.java
+++ src/lab6/SinglyLinkedList.java
@@ -39,6 +39,7 @@ public class SinglyLinkedList<E> {
 		Node<E> newNode = new Node<>(e);
 		if (isEmpty()) {
 			head = newNode;
+			tail = newNode;
 		} else {
 			newNode.setNext(head);
 			head = newNode;
@@ -50,16 +51,65 @@ public class SinglyLinkedList<E> {
 		Node<E> newNode = new Node<>(e);
 		if (isEmpty()) {
 			tail = newNode;
+			head = newNode;
 		} else {
 			newNode.setNext(tail);
 			tail = newNode;
 		}
 		size++;
 	}
+
 	public E removeFirst() {
-		
+		if (isEmpty()) {
+			return null;
+		}
+		E removeElements = head.getData();
+		head = head.getNext();
+		size--;
+		if (isEmpty()) {
+			tail = null;
+		}
+		return removeElements;
 	}
-public E removeLast() {
-		
+
+	public E removeLast() {
+		E removeElement;
+		if (isEmpty()) {
+			return null;
+		} else if (head == tail) {
+			removeElement = head.getData();
+			head = null;
+			tail = null;
+		} else {
+			Node<E> checkElement = head;
+
+			while (checkElement.getNext() != null) {
+
+				checkElement = checkElement.getNext();
+			}
+
+			removeElement = tail.getData();
+			tail = checkElement;
+
+			size--;
+
+		}
+		return removeElement;
+
+	}
+
+	public static void main(String[] args) {
+		SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+		list.adFirst(2);
+		list.adFirst(1);
+		list.addLast(6);
+		list.addLast(5);
+		System.out.println("First Element:" + list.first());
+		System.out.println("Last Element:" + list.last());
+		System.out.println("Size:" + list.size());
+		System.out.println("RemovedFirst Element:" + list.removeFirst());
+		System.out.println("RemovedLast Element:" + list.removeLast());
+		System.out.println("Size after remove:" + list.size());
+
 	}
 }
