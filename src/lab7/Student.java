9937ddca5ba43b87292a1ceabb9f61b537cb5306
win10pro <win10pro@DESKTOP-DG8QK26>
2023-10-29 13:17:24 -0700

    them

--- src/lab7/Student.java
+++ src/lab7/Student.java
@@ -1,5 +1,40 @@
 package lab7;
 
 public class Student {
+	private String id;
+	private String firstName;
+	private String lastName;
+	private int birthYear;
+	private double GPA;
+	public Student(String id, String firstName, String lastName, int birthYear, double gPA) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthYear = birthYear;
+		GPA = gPA;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public double getGPA() {
+		return GPA;
+	}
+	@Override
+	public String toString() {
+		return "id" + id + ", firstName" + firstName + ", lastName" + lastName + ", birthYear" + birthYear
+				+ ", GPA" + GPA;
+	}
+	
+	
 
 }
---
+++ src/lab7/Test.java
@@ -0,0 +1,9 @@
+package lab7;
+
+public class Test {
+	public static void main(String[] args) {
+		clazz cl = new clazz();
+		cl.loadStudents("fol/Student.txt");
+	}
+
+}
